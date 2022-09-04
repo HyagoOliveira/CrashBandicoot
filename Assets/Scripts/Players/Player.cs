9c415d8c97d8ff5429e8e00af556139bdf1a7a86
Hyago Oliveira <hyagogow@gmail.com>
2022-09-04 12:27:49 -0300

    Improve Player scripts

--- Assets/Scripts/Players/Player.cs
+++ Assets/Scripts/Players/Player.cs
@@ -1,10 +1,73 @@
+using System;
 using UnityEngine;
 
 namespace CrashBandicoot.Players
 {
     [DisallowMultipleComponent]
-    public sealed class Player : MonoBehaviour
+    public sealed class Player : MonoBehaviour, IEnable, IDisable, IEquatable<Player>
     {
+        [SerializeField, Tooltip("The player identifier.")]
+        private PlayerName id = PlayerName.None;
 
+        /// <summary>
+        /// The player identifier name.
+        /// </summary>
+        public PlayerName Name => id;
+
+        /// <summary>
+        /// Places the player at the given position and rotation.
+        /// </summary>
+        /// <param name="position">World position.</param>
+        /// <param name="rotation">World rotation.</param>
+        public void Place(Vector3 position, Quaternion rotation)
+        {
+            transform.SetPositionAndRotation(position, rotation);
+        }
+
+        public void Switch(Player nextPlayer)
+        {
+            var position = nextPlayer.transform.position;
+            var rotation = nextPlayer.transform.rotation;
+
+            transform.SetPositionAndRotation(position, rotation);
+        }
+
+        /// <summary>
+        /// Enables the player.
+        /// </summary>
+        public void Enable()
+        {
+            gameObject.SetActive(true);
+            //TODO enable all necessary components.
+        }
+
+        /// <summary>
+        /// Disables the player.
+        /// </summary>
+        public void Disable()
+        {
+            gameObject.SetActive(false);
+            //TODO disable all necessary components.
+        }
+
+        /// <summary>
+        /// Checks if is able to switch.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsAbleToSwitch() => !gameObject.activeInHierarchy;
+        //TODO && check if spawn animation is finished
+
+        /// <summary>
+        /// Checks if the given player is equals to this.
+        /// </summary>
+        /// <param name="other">The player to test.</param>
+        /// <returns>True if both players are the same. False otherwise.</returns>
+        public bool Equals(Player other) => Name == other.Name;
+
+        internal void Destroy()
+        {
+            Destroy(gameObject);
+            // TODO dispose other components here.
+        }
     }
 }
--- Assets/Scripts/Players/PlayerSettings.cs
+++ Assets/Scripts/Players/PlayerSettings.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CrashBandicoot.Players
@@ -5,6 +7,207 @@ namespace CrashBandicoot.Players
     [CreateAssetMenu(fileName = "PlayerSettings", menuName = Constants.Path.Player + "Settings", order = 110)]
     public sealed class PlayerSettings : ScriptableObject
     {
+        [SerializeField, Tooltip("The first Player to be spawned.")]
+        private PlayerName first = PlayerName.CrashBandicoot;
+        [SerializeField, Tooltip("The Players prefabs.")]
+        private GameObject[] prefabs;
 
+        /// <summary>
+        /// Action fired when a Player is spawned.
+        /// </summary>
+        public event Action<Player> OnPlayerSpawn;
+
+        /// <summary>
+        /// Action fired when a Player is switched.
+        /// <para>
+        /// <b>First param</b>: Current Player.<br/>
+        /// <b>Second param</b>: Last Player.
+        /// </para>
+        /// </summary>
+        public event Action<Player, Player> OnPlayerSwitch;
+
+        /// <summary>
+        /// Action fired when a Player dies.
+        /// </summary>
+        public event Action<Player> OnPlayerDestroyed;
+
+        /// <summary>
+        /// The last active Player.
+        /// </summary>
+        public Player Last => players[lastName];
+
+        /// <summary>
+        /// The current active Player.
+        /// </summary>
+        public Player Current => players[currentName];
+
+        private PlayerName lastName;
+        private PlayerName currentName;
+        private Dictionary<PlayerName, Player> players;
+
+        internal void Initialize() => InstantiatePrefabs();
+
+        /// <summary>
+        /// Spawns the first Player only if no other one is enabled.
+        /// </summary>
+        public void Spawn()
+        {
+            var enabledPlayer = FindObjectOfType<Player>(includeInactive: false);
+            var hasNoPlayerEnabled = enabledPlayer == null;
+            if (hasNoPlayerEnabled) Spawn(first, Vector3.zero, Quaternion.identity);
+        }
+
+        /// <summary>
+        /// Spawns the given Player using the position and rotation.
+        /// </summary>
+        /// <param name="player">The Player to spawn.</param>
+        /// <param name="position">The world position to spawn.</param>
+        /// <param name="rotation">The world rotation to spawn.</param>
+        public void Spawn(PlayerName player, Vector3 position, Quaternion rotation)
+        {
+            lastName = PlayerName.None;
+            currentName = player;
+
+            Current.Enable();
+            Current.Place(position, rotation);
+
+            HandleSpawn();
+        }
+
+        /// <summary>
+        /// UnSpawns the current Player.
+        /// </summary>
+        public void UnSpawn()
+        {
+            //TODO
+        }
+
+        /// <summary>
+        /// Switches into the next Player.
+        /// <para>It checks if switch is possible.</para>
+        /// </summary>
+        public void Switch() => Switch(GetNextPlayerName());
+
+        /// <summary>
+        /// Switches into the given player if available.
+        /// </summary>
+        /// <param name="name">The Player to switch.</param>
+        public void Switch(PlayerName name)
+        {
+            if (IsAbleToSwitchFor(name, out Player nextPlayer))
+            {
+                Current.Switch(nextPlayer);
+
+                lastName = currentName;
+                currentName = name;
+
+                HandleSwitch();
+            }
+        }
+
+        /// <summary>
+        /// Destroys the given player.
+        /// </summary>
+        /// <param name="name">The Player to destroy.</param>
+        public void Destroy(PlayerName name)
+        {
+            OnPlayerDestroyed?.Invoke(players[name]);
+            players[name].Destroy();
+            players[name] = null;
+        }
+
+        /// <summary>
+        /// Disables all the instantiated Players.
+        /// </summary>
+        public void Disable()
+        {
+            foreach (var player in players.Values)
+            {
+                player.Disable();
+            }
+        }
+
+        /// <summary>
+        /// Gets the next Player name based on the current one.
+        /// </summary>
+        /// <returns></returns>
+        public PlayerName GetNextPlayerName()
+        {
+            //TODO improve this part using a proper Indexed Dictionary collection.
+            var index = -1;
+            var names = new PlayerName[players.Count];
+            players.Keys.CopyTo(names, index: 0);
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (currentName == names[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (++index >= names.Length) index = 0;
+            return names[index];
+        }
+
+        /// <summary>
+        /// Checks if contains the given player and references it back.
+        /// </summary>
+        /// <param name="name">The Player name trying to get.</param>
+        /// <param name="player">The Player if available.</param>
+        /// <returns>Whether contains the player.</returns>
+        public bool Contains(PlayerName name, out Player player) =>
+            players.TryGetValue(name, out player);
+
+        /// <summary>
+        /// Checks if is able to switch into the given player.
+        /// </summary>
+        /// <param name="name">The Player name trying to switch.</param>
+        /// <param name="player">The Player if available.</param>
+        /// <returns>Whether is possible to switch for the given player.</returns>
+        public bool IsAbleToSwitchFor(PlayerName name, out Player player)
+        {
+            var hasPlayer = Contains(name, out player);
+            return hasPlayer && player.IsAbleToSwitch();
+        }
+
+        private void InstantiatePrefabs()
+        {
+            var scenePlayers = GetScenePlayers();
+            players = new Dictionary<PlayerName, Player>(prefabs.Length);
+
+            foreach (var prefab in prefabs)
+            {
+                var prefabPlayerName = prefab.GetComponent<Player>().Name;
+                var hasScenePlayer = scenePlayers.TryGetValue(prefabPlayerName, out Player player);
+
+                if (!hasScenePlayer)
+                {
+                    var instance = Instantiate(prefab, position: Vector3.zero, rotation: Quaternion.identity);
+                    player = instance.GetComponent<Player>();
+
+                    player.gameObject.name = prefab.name;
+                    player.gameObject.SetActive(false);
+                }
+
+                players.Add(player.Name, player);
+            }
+        }
+
+        private Dictionary<PlayerName, Player> GetScenePlayers()
+        {
+            var scenePlayers = new Dictionary<PlayerName, Player>();
+
+            foreach (var player in FindObjectsOfType<Player>(includeInactive: true))
+            {
+                scenePlayers.Add(player.Name, player);
+            }
+
+            return scenePlayers;
+        }
+
+        private void HandleSpawn() => OnPlayerSpawn?.Invoke(Current);
+
+        private void HandleSwitch() => OnPlayerSwitch?.Invoke(Current, Last);
     }
 }
