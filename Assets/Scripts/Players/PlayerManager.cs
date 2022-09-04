39697f78323521954b64cd82026a7de8629e5b9f
Hyago Oliveira <hyagogow@gmail.com>
2022-09-04 12:14:46 -0300

    Refactor to PlayerManager

--- Assets/Scripts/Players/PlayerManager.cs
+++ Assets/Scripts/Players/PlayerManager.cs
@@ -5,6 +5,11 @@ namespace CrashBandicoot.Players
     [DisallowMultipleComponent]
     public sealed class PlayerManager : MonoBehaviour
     {
+        [SerializeField] private PlayerSettings settings;
+        [SerializeField] private PlayerInputSettings inputSettings;
 
+        private void Awake() => inputSettings.Initialize();
+        private void OnEnable() => inputSettings.EnableActions();
+        private void OnDisable() => inputSettings.DisableActions();
     }
 }
