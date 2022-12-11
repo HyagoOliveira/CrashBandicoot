f88549f5eafaa0a8e755055b9e7d36137949f9db
Hyago Oliveira <hyagogow@gmail.com>
2022-12-11 17:15:59 -0300

    Add RunState

---
+++ Assets/Scripts/Players/States/RunState.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using ActionCode.AnimatorStates;
+using CrashBandicoot.Physicss;
+using CrashBandicoot.Animations;
+
+namespace CrashBandicoot.Players
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(CharacterMotor))]
+    [RequireComponent(typeof(CharacterMotorAnimator))]
+    public sealed class RunState : AbstractMonoBehaviourState
+    {
+        [SerializeField] private CharacterMotor motor;
+        [SerializeField] private CharacterMotorAnimator animator;
+
+        private void Reset()
+        {
+            motor = GetComponent<CharacterMotor>();
+            animator = GetComponent<CharacterMotorAnimator>();
+        }
+
+        protected override void UpdateState()
+        {
+            base.UpdateState();
+            UpdateAnimationAccordingWithSlopeInclination();
+        }
+
+        protected override void ExitState() => animator.ResetRunningSlope();
+
+        private void UpdateAnimationAccordingWithSlopeInclination()
+        {
+            if (motor.GroundHit.collider != null)
+            {
+                var dot = Vector3.Dot(motor.GroundHit.normal, transform.forward);
+                var isRunningUpwardSlope = dot < 0F;
+                var isRunningDownwardSlope = dot > 0F;
+
+                if (isRunningUpwardSlope) animator.SetRunningSlopeUpwards();
+                else if (isRunningDownwardSlope) animator.SetRunningSlopeDownwards();
+                else animator.ResetRunningSlope();
+            }
+            else animator.ResetRunningSlope();
+        }
+    }
+}
