132045e87f202099fc4bef2b80e3287c1332d532
Hyago Oliveira <hyagogow@gmail.com>
2022-12-10 23:37:23 -0300

    Add AttachPlayerToCinemachineCamera

---
+++ Assets/Scripts/Players/Cameras/AttachPlayerToCinemachineCamera.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace CrashBandicoot.Players
+{
+    [DisallowMultipleComponent]
+    public abstract class AttachPlayerToCinemachineCamera : MonoBehaviour
+    {
+        [SerializeField] private PlayerSettings settings;
+
+        private void Reset() => FindCamera();
+        private void OnEnable() => AddListeners();
+        private void OnDisable() => RemoveListeners();
+
+        protected abstract void FindCamera();
+        protected abstract void SwitchCameraTo(Player player);
+
+        private void AddListeners()
+        {
+            settings.OnPlayerSpawn += HandleOnPlayerSpawn;
+            settings.OnPlayerSwitch += HandleOnPlayerSwitch;
+        }
+
+        private void RemoveListeners()
+        {
+            settings.OnPlayerSpawn -= HandleOnPlayerSpawn;
+            settings.OnPlayerSwitch -= HandleOnPlayerSwitch;
+        }
+
+        private void HandleOnPlayerSpawn(Player player) => SwitchCameraTo(player);
+        private void HandleOnPlayerSwitch(Player player, Player _) => SwitchCameraTo(player);
+    }
+}
