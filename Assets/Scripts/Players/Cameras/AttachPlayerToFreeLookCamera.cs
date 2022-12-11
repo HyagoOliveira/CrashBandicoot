0e2aa06209d94c41ed4f29bedb29328d320e59a3
Hyago Oliveira <hyagogow@gmail.com>
2022-12-10 23:38:01 -0300

    Refact AttachPlayerToFreeLookCamera

--- Assets/Scripts/Players/Cameras/AttachPlayerToFreeLookCamera.cs
+++ Assets/Scripts/Players/Cameras/AttachPlayerToFreeLookCamera.cs
@@ -3,34 +3,14 @@ using Cinemachine;
 
 namespace CrashBandicoot.Players
 {
-    [DisallowMultipleComponent]
     [RequireComponent(typeof(CinemachineFreeLook))]
-    public sealed class AttachPlayerToFreeLookCamera : MonoBehaviour
+    public sealed class AttachPlayerToFreeLookCamera : AttachPlayerToCinemachineCamera
     {
-        [SerializeField] private PlayerSettings settings;
         [SerializeField] private CinemachineFreeLook virtualCamera;
 
-        private void Reset() => virtualCamera = GetComponent<CinemachineFreeLook>();
+        protected override void FindCamera() => virtualCamera = GetComponent<CinemachineFreeLook>();
 
-        private void OnEnable() => AddListeners();
-        private void OnDisable() => RemoveListeners();
-
-        private void AddListeners()
-        {
-            settings.OnPlayerSpawn += HandleOnPlayerSpawn;
-            settings.OnPlayerSwitch += HandleOnPlayerSwitch;
-        }
-
-        private void RemoveListeners()
-        {
-            settings.OnPlayerSpawn -= HandleOnPlayerSpawn;
-            settings.OnPlayerSwitch -= HandleOnPlayerSwitch;
-        }
-
-        private void HandleOnPlayerSpawn(Player player) => SwitchCameraTo(player);
-        private void HandleOnPlayerSwitch(Player player, Player _) => SwitchCameraTo(player);
-
-        private void SwitchCameraTo(Player player)
+        protected override void SwitchCameraTo(Player player)
         {
             virtualCamera.Follow = player.transform;
             virtualCamera.LookAt = player.transform;
