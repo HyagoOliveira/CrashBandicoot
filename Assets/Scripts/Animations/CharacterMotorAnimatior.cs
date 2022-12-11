f76e7f9eb1baddb3391b6e15f17d96561b27ea69
Hyago Oliveira <hyagogow@gmail.com>
2022-12-11 16:42:08 -0300

    Rename parameter HillIndex -> RunningSlopeIndex

--- Assets/Scripts/Animations/CharacterMotorAnimatior.cs
+++ Assets/Scripts/Animations/CharacterMotorAnimatior.cs
@@ -11,11 +11,11 @@ namespace CrashBandicoot.Animators
         [SerializeField] private Animator animator;
         [SerializeField] private CharacterMotor motor;
 
-        private readonly int hillIndex = Animator.StringToHash("HillIndex");
         private readonly int isGrounded = Animator.StringToHash("IsGrounded");
         private readonly int isAirborne = Animator.StringToHash("IsAirborne");
         private readonly int isMoveInputting = Animator.StringToHash("IsMoveInputting");
         private readonly int respawn = Animator.StringToHash("Respawn");
+        private readonly int runningSlopeIndex = Animator.StringToHash("RunningSlopeIndex");
 
         private void Reset()
         {
@@ -32,9 +32,9 @@ namespace CrashBandicoot.Animators
 
         public void Respawn() => animator.SetTrigger(respawn);
 
-        private void SetHillIndex(float value) => animator.SetFloat(hillIndex, value);
         private void SetIsGrounded(bool value) => animator.SetBool(isGrounded, value);
         private void SetIsAirborne(bool value) => animator.SetBool(isAirborne, value);
         private void SetIsMoveInputting(bool value) => animator.SetBool(isMoveInputting, value);
+        private void SetRunningSlopeIndex(float value) => animator.SetFloat(runningSlopeIndex, value);
     }
 }
