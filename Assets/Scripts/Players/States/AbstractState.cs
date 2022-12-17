13755b36bd8bc78f86f729bc5bb685a88f2d6899
Hyago Oliveira <hyagogow@gmail.com>
2022-12-17 18:32:09 -0300

    Add AbstractState

---
+++ Assets/Scripts/Players/States/AbstractState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using ActionCode.AnimatorStates;
+using CrashBandicoot.Physicss;
+
+namespace CrashBandicoot.Players
+{
+	[RequireComponent(typeof(CharacterMotor))]
+	[RequireComponent(typeof(PlayerAnimator))]
+	public abstract class AbstractState : AbstractMonoBehaviourState 
+	{
+		[SerializeField] protected CharacterMotor motor;
+		[SerializeField] protected PlayerAnimator animator;
+
+		protected virtual void Reset()
+		{
+			motor = GetComponent<CharacterMotor>();
+			animator = GetComponent<PlayerAnimator>();
+		}
+	}
+}
