c1177a592be025d9057b6085123fc86d53f26209
Hyago Oliveira <hyagogow@gmail.com>
2022-12-29 13:22:41 -0300

    Add SpinState

---
+++ Assets/Scripts/Players/States/SpinState.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace CrashBandicoot.Players
+{
+    [DisallowMultipleComponent]
+    public sealed class SpinState : AbstractState
+    {
+        [SerializeField, Tooltip("The Spin time (in seconds)."), Min(0F)]
+        private float time = 0.5F;
+        [SerializeField, Tooltip("The Cooldown time (in seconds)."), Min(0F)]
+        private float cooldown = 0.5F;
+        [SerializeField]
+        private GameObject spinVFX;
+
+        private float lastTime;
+
+        protected override void EnterState()
+        {
+            base.EnterState();
+            spinVFX.SetActive(true);
+        }
+
+        protected override void ExitState()
+        {
+            base.ExitState();
+            spinVFX.SetActive(false);
+        }
+
+        public void UpdateInput(bool isButtonDown)
+        {
+            //animator.SetSpinning(isButtonDown);
+            if (!isButtonDown || !CanSpin()) return;
+
+            Enable();
+            Invoke(nameof(Disable), time);
+        }
+
+        private bool CanSpin() => !IsExecuting && !IsCooldown();
+
+        private bool IsCooldown()
+        {
+            var elapsedTime = GetTime() - lastTime;
+            return elapsedTime <= cooldown;
+        }
+
+        private void Enable() => animator.SetSpinning(true);
+
+        private void Disable()
+        {
+            lastTime = GetTime();
+            animator.SetSpinning(false);
+        }
+    }
+}
