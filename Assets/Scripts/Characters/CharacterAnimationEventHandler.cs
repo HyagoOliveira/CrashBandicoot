af05a1dc095af39ea64dad60ef051a49b1f5a059
Hyago Oliveira <hyagogow@gmail.com>
2023-01-02 22:34:35 -0300

    CharacterAnimationEventHandler

---
+++ Assets/Scripts/Characters/CharacterAnimationEventHandler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace CrashBandicoot.Characters
+{
+    /// <summary>
+    /// Character Handler for Events inside AnimationClips.
+    /// <para>Events inside AnimationClips call functions inside this component.</para>
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class CharacterAnimationEventHandler : MonoBehaviour
+    {
+    }
+}
