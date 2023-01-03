d61e7afaf67bb4871c56a18e60a3b009f4b276ae
Hyago Oliveira <hyagogow@gmail.com>
2023-01-03 15:56:52 -0300

    Improve Players structures

--- Assets/Scripts/Players/States/SpinState.cs
+++ Assets/Scripts/Players/States/SpinState.cs
@@ -1,28 +1,32 @@
 using UnityEngine;
-using CrashBandicoot.Characters;
 
 namespace CrashBandicoot.Players
 {
     [DisallowMultipleComponent]
+    [RequireComponent(typeof(PlayerStructure))]
     public sealed class SpinState : AbstractState
     {
         [SerializeField, Tooltip("The Spin time (in seconds)."), Min(0F)]
         private float time = 0.5F;
         [SerializeField, Tooltip("The Cooldown time (in seconds)."), Min(0F)]
         private float cooldown = 0.5F;
-        [SerializeField]
-        private GameObject spinVFX;
-
-        [SerializeField] private CharacterAudioSources audioSource;
+        [SerializeField] private GameObject spinVFX;
+        [SerializeField] private PlayerStructure structure;
         [SerializeField] private AudioClip spinAttack;
 
         private float lastTime;
+
+        protected override void Reset()
+        {
+            base.Reset();
+            structure = GetComponent<PlayerStructure>();
+        }
 
         protected override void EnterState()
         {
             base.EnterState();
             spinVFX.SetActive(true);
-            audioSource.PlayOnChest(spinAttack);
+            structure.PlayOnChest(spinAttack);
         }
 
         protected override void ExitState()
